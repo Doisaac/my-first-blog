11ecd0ae4d5f47fdddde9711fb3a765b4c7a7458
douglas <douglasbarrera95@gmail.com>
2020-04-18 20:12:11 -0400

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
